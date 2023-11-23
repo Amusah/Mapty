626e8b8d30785a0ccf68e848ed88b6b362083648
Amusah <henryamusah@gmail.com>
2023-11-23 08:28:12 +0000

    recording number of clicks on workout

--- App/script.js
+++ App/script.js
@@ -12,6 +12,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 class Workout {
     date = new Date();
     id = (Date.now() + '').slice(-10);
+    clicks = 0;
 
     constructor(coords, distance, duration){
         this.coords = coords; // [lattitude, longitude]
@@ -24,6 +25,10 @@ class Workout {
         const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
         this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+    }
+
+    click(){
+        this.clicks++;
     }
 }
 
@@ -247,7 +252,10 @@ class App {
             pan: {
                 duration: 1
             }
-        })
+        });
+
+        // using the public interface
+        workout.click();
     }
 }
 const app = new App();
